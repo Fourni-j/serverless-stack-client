0902226663a5fcf9928f98e4c0d15163b4505e92
Charles-Adrien Fournier <charles@Charles-Adriens-MacBook-Pro-1.home>
2020-06-16 10:55:38 +0200

    Adding a Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,18 +1,42 @@
-export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
     STRIPE_KEY: "pk_test_51GuH9oHhAZopZSwIrLldqhzPinpKceP7Fa4XzCiWsmi9mSMfUaRJlVgscRMBKIsKFVREIuEnJL2XFqFRdr5k1lFU0068XUzsRj",
     s3: {
-        REGION: "eu-west-1",
-        BUCKET: "notes-app-uploads-react-tuto"
+        REGION: "us-east-1",
+        BUCKET: "notes-app-2-api-dev-attachmentsbucket-nlyo1jwbpkgx"
     },
     apiGateway: {
-        REGION: "eu-west-1",
-        URL: "https://ribiw0ibu8.execute-api.eu-west-1.amazonaws.com/prod"
+        REGION: "us-east-1",
+        URL: "https://1merzxqad3.execute-api.us-east-1.amazonaws.com/dev"
     },
     cognito: {
-        REGION: "eu-west-1",
-        USER_POOL_ID: "eu-west-1_YTucIR8xy",
-        APP_CLIENT_ID: "4kqb5t63s99pr2pb8tqepgq5du",
-        IDENTITY_POOL_ID: "eu-west-1:1b3e167b-9f3d-42bc-978e-07fa687cd2d3"
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_D8T1L90sp",
+        APP_CLIENT_ID: "66470tggo6i29qe8fssddl1na",
+        IDENTITY_POOL_ID: "us-east-1:87407f9c-ba81-4a6f-82bb-12f36f207b12"
     }
+};
+
+const prod = {
+    STRIPE_KEY: "pk_test_51GuH9oHhAZopZSwIrLldqhzPinpKceP7Fa4XzCiWsmi9mSMfUaRJlVgscRMBKIsKFVREIuEnJL2XFqFRdr5k1lFU0068XUzsRj",
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-2-api-prod-attachmentsbucket-mo24g4k03u85"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://8gvumr3xta.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_s0vSV7yPL",
+        APP_CLIENT_ID: "1m5jh6e3ntuerm83dadubbcoaq",
+        IDENTITY_POOL_ID: "us-east-1:7dd26c3d-044d-4fe7-a66f-07b3da8923be"
+    }
+};
+
+const config = process.evn.REACT_APP_STAGE === 'prod' ? prod : dev;
+
+export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
 };
